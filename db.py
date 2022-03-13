f1e868679863acf863abe303db7aacc402f11729
YK <kai.yoshi.v2@gmail.com>
2022-03-13 15:13:51 +0900

    fixed
    2

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from sqlmodel import Session, create_engine
 import os
-database_url = os.environ.get('DATABASE_URL','sqlite:///data.db?check_same_thread=False').replace("postgres://","postgresql://")
+database_url = os.environ.get('DATABASE_URL').replace("postgres://","postgresql://")
+# database_url = os.environ.get('DATABASE_URL','sqlite:///data.db?check_same_thread=False').replace("postgres://","postgresql://")
 engine = create_engine(database_url,echo=False)
