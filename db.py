40495191ec10f3f52dc77eeca8e8d3d6686b7478
YK <kai.yoshi.v2@gmail.com>
2022-03-21 16:29:46 +0900

    tweaked a little bit

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from sqlmodel import Session, create_engine
+from env_vars import database_url
 import os
-# database_url = os.environ.get('DATABASE_URL').replace("postgres://","postgresql://")
-# database_url = os.getenv('DATABASE_URL','sqlite:///data.db?check_same_thread=False').replace("postgres://","postgresql://")
-database_url = 'postgresql://ztgsuxglqxgqaf:87f6b30fe550286ea4d05cde3c4ff3408dbc2d0c1562119dbab6abd773535e05@ec2-54-236-137-173.compute-1.amazonaws.com:5432/dkk1q1rsknfg7'
+
 engine = create_engine(database_url,echo=False)
